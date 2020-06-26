a3bee937171bf9cd476bef4fabc5e1836365c83b
Kyubendo <kotigo111@gmail.com>
2020-06-26 10:44:23 +0300

    complete powerSum

--- powerSum.js
+++ powerSum.js
@@ -6,29 +6,30 @@ function powerSum(X, N) {
         arr.push(Math.pow(i,N))
     }
     let resCount = 0
-    function comb(cur, rest) {
-
-        if (rest.length===0) {
-            return
-        }
-        comb(cur.concat(rest[0]), rest.slice(1))
-
+    function checkSum(cur) {
         let sum = 0
         for (let i=0;i<cur.length;i++){
             sum+=cur[i]
-            if (sum>X)return;
+            if (sum>X) return true
         }
         if (sum===X) {
             resCount++;
-            return;
+            return true;
         }
+    }
+    function comb(cur, rest) {
+        if (rest.length===0) {
+            checkSum(cur)
+            return
+        }
+        comb(cur.concat(rest[0]), rest.slice(1))
+        if (checkSum(cur)) return;
         comb(cur, rest.slice(1))
     }
     comb([], arr)
-    if (arr[arr.length-1]===X)resCount++
     return resCount
 }
 const t0 = performance.now()
-console.log(powerSum(100, 2))
+console.log(powerSum(1000, 2))
 const t1 = performance.now()
 console.log(~~(t1-t0)/1000)
